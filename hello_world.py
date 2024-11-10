6d3ce98d96a5e206a30b94a3a977d64fcf5eeb37
Ville <ville.vartiainen@tuni.fi>
2024-11-10 11:19:35 +0200

    Amend fix to merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,3 @@
 print("Hello world!")
-<<<<<<< HEAD
 print("Hello darkness my old friend")
-=======
 print("I've come to talk with you again")
->>>>>>> intermediate-merge-conflict/feature/modify-print
